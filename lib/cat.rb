cf49ba4f4ee84ab6ccc3678af9d08aeb4ab720f2
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-21 00:13:28 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,8 +1,18 @@
 class Cat
-  attr_accessor :name
+  attr_accessor :mood, :owner
+  attr_reader :name
 
-  def initialize(name)
-    @name = name
+@@all = []
 
-  end
+  def initialize(name, owner)
+  @name = name
+  @mood = "nervous"
+  @owner = owner
+  @@all << self
+  owner.cats << self
+end
+
+ def self.all 
+  @@all 
+ end
 end
